662aa3f2f366e368d23a88525aec6281c8fe52cb
StoneHappy <18017158729@163.com>
2022-11-04 21:26:30 +0800

    ✨ feat: date lcd

--- src/Runtime/Series/g_ExDateTime/ExDateTime.cpp
+++ src/Runtime/Series/g_ExDateTime/ExDateTime.cpp
@@ -1,11 +1,24 @@
-#include "ExDateTime.h"
+﻿#include "ExDateTime.h"
 #include "ui_ExDateTime.h"
+#include <iostream>
+#include <qdatetime.h>
+#include <QTimer>
+#include <qobjectdefs.h>
+#include <qwindowdefs.h>
 
 ExDateTime::ExDateTime(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::ExDateTime)
 {
     ui->setupUi(this);
+
+    setWindowTitle(QString::fromLocal8Bit("时间日期(QTime/QDate/QDateTime)和定时器(QTimer)"));
+
+    m_timer = new QTimer(this);
+    m_timer->stop();
+    m_timer->setInterval(1000);
+
+    connect(m_timer, SIGNAL(timeout()), this, SLOT(onTimerOut()));;
 }
 
 ExDateTime::~ExDateTime()
@@ -26,4 +39,52 @@ void ExDateTime::on_btnGetDateTime_clicked()
     ui->dateTimeEdit->setDateTime(currDateTime);
     ui->editDateTime->setText(currDateTime.toString("yyyy-MM-dd hh:mm:ss:zzz"));
     ui->labCurrDataTime->setText(currDateTime.toString("yyyy-MM-dd hh:mm:ss:zzz"));
+}
+
+
+void ExDateTime::on_btnStatrt_clicked()
+{
+    m_timer->start();
+    m_time.start();
+
+    ui->btnStatrt->setEnabled(false);
+    ui->btnStop->setEnabled(true);
+    ui->btnPeriod->setEnabled(false);
+
+    ui->labGo->setText(QString::fromLocal8Bit("时间流逝在后台计算中..."));
+}
+
+void ExDateTime::on_btnStop_clicked()
+{
+    m_timer->stop();
+    ui->btnStop->setEnabled(false);
+    ui->btnStatrt->setEnabled(true);
+
+    int tmMsec = m_time.elapsed();
+
+    int ms = tmMsec % 1000;
+    int sec = tmMsec / 1000;
+    ui->btnPeriod->setEnabled(true);
+    ui->labGo->setText(QString::fromLocal8Bit("时间已经流逝：%1 秒 %2 毫秒").arg(sec).arg(ms));
+}
+
+void ExDateTime::on_btnPeriod_clicked()
+{
+    m_timer->setInterval(ui->spinBox->value());
+}
+
+
+
+void ExDateTime::onTimerOut()
+{
+    QTime currTime = QTime::currentTime();
+    ui->lcdHH->display(currTime.toString("hh"));
+    ui->lcdmm->display(currTime.toString("mm"));
+    ui->lcdSS->display(currTime.toString("ss"));
+
+    int val = ui->progressBar->value();
+    val++;
+    val = val > 100 ? 0 : val;
+
+    ui->progressBar->setValue(val);
 }
--- src/Runtime/Series/g_ExDateTime/ExDateTime.h
+++ src/Runtime/Series/g_ExDateTime/ExDateTime.h
@@ -2,7 +2,9 @@
 #define EXDATETIME_H
 
 #include <QWidget>
+#include <qdatetime.h>
 #include <qobjectdefs.h>
+#include <qwindowdefs.h>
 
 namespace Ui {
 class ExDateTime;
@@ -18,9 +20,17 @@ public:
 
 private slots:
     void on_btnGetDateTime_clicked();
+    void on_btnStatrt_clicked();
+    void on_btnStop_clicked();
+    void on_btnPeriod_clicked();
+
+    void onTimerOut();   //处理计时器的信号的槽函数
 
 private:
     Ui::ExDateTime *ui;
+
+    QTimer* m_timer;     //定时器（不可见控件）
+    QTime   m_time;      //计时器（此处用作）
 };
 
 #endif // EXDATETIME_H
