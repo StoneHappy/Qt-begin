ecf1d0003e4fc18fdca4e3e83b197712791edc15
ImGili <18017158729@163.com>
2022-11-08 12:54:58 +0800

    🎉 init: Tree

--- src/Runtime/Series/j_ExQTreeWidget/ExQTreeWidget.cpp
+++ src/Runtime/Series/j_ExQTreeWidget/ExQTreeWidget.cpp
@@ -1,12 +1,44 @@
-#include "ExQTreeWidget.h"
+﻿#include "ExQTreeWidget.h"
 #include "ui_ExQTreeWidget.h"
+#include <qicon.h>
+#include <qnamespace.h>
+#include <qtreewidget.h>
+#include <qvariant.h>
 
 ExQTreeWidget::ExQTreeWidget(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::ExQTreeWidget)
 {
     ui->setupUi(this);
+
+    setWindowTitle(QString::fromLocal8Bit("QTreeWidget和QDockWidget的讲解和使用"));
+
+    setCentralWidget(ui->scrollArea);
+    initTree();
+    
 }
+
+void ExQTreeWidget::initTree()
+{
+    ui->treeFiles->clear();
+
+
+    QString dataStr = "";
+    QIcon icon;
+
+    icon.addFile(":/images/Image001.jpg");
+
+    QTreeWidgetItem* root = new QTreeWidgetItem(treeItemType::itemRoot);
+    root->setIcon(treeColNum::colItem, icon);
+    root->setText(treeColNum::colItem, QString::fromLocal8Bit("相簿"));
+    root->setText(treeColNum::colItemType, QString::fromLocal8Bit("treeItemType"));
+    root->setFlags(Qt::ItemIsSelectable | Qt::ItemIsUserCheckable | Qt::ItemIsEnabled | Qt::ItemIsAutoTristate);
+    root->setCheckState(treeColNum::colItem, Qt::Unchecked);
+    root->setData(treeColNum::colItem, Qt::UserRole, QVariant(dataStr));
+
+    ui->treeFiles->addTopLevelItem(root);
+}
+
 
 ExQTreeWidget::~ExQTreeWidget()
 {
--- src/Runtime/Series/j_ExQTreeWidget/ExQTreeWidget.h
+++ src/Runtime/Series/j_ExQTreeWidget/ExQTreeWidget.h
@@ -2,6 +2,7 @@
 #define EXQTREEWIDGET_H
 
 #include <QMainWindow>
+#include <qobjectdefs.h>
 
 namespace Ui {
 class ExQTreeWidget;
@@ -12,8 +13,23 @@ class ExQTreeWidget : public QMainWindow
     Q_OBJECT
 
 public:
+    enum treeItemType {         //枚举，节点类型
+        itemRoot,
+        itemFile,
+        itemImage
+    };
+
+     enum treeColNum {           //目录树列表的编号
+        colItem = 0,
+        colItemType = 1
+    };
+
+public:
     explicit ExQTreeWidget(QWidget *parent = nullptr);
     ~ExQTreeWidget();
+
+private slots:
+    void initTree();
 
 private:
     Ui::ExQTreeWidget *ui;
