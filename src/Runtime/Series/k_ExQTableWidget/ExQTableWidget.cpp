e838aa699ec318247888cc38d4286501507d12f6
ImGili <18017158729@163.com>
2022-11-09 09:43:56 +0800

    ✨ feat: status bar

--- src/Runtime/Series/k_ExQTableWidget/ExQTableWidget.cpp
+++ src/Runtime/Series/k_ExQTableWidget/ExQTableWidget.cpp
@@ -1,4 +1,4 @@
-#include "ExQTableWidget.h"
+﻿#include "ExQTableWidget.h"
 #include "ui_ExQTableWidget.h"
 
 ExQTableWidget::ExQTableWidget(QWidget *parent) :
@@ -6,6 +6,18 @@ ExQTableWidget::ExQTableWidget(QWidget *parent) :
     ui(new Ui::ExQTableWidget)
 {
     ui->setupUi(this);
+    setWindowTitle(QString::fromLocal8Bit("QTableWidget的讲解和使用"));
+
+    m_labCellIndex = new QLabel(QString::fromLocal8Bit("当前单元格坐标："), this);          //状态栏初始化
+    m_labCellIndex->setMinimumWidth(250);
+    m_labCellType = new QLabel(QString::fromLocal8Bit("当前单元格类型："), this);
+    m_labCellType->setMinimumWidth(200);
+    m_labStudID = new QLabel(QString::fromLocal8Bit("学生ID："), this);
+    m_labStudID->setMinimumWidth(200);
+    ui->statusbar->addWidget(m_labCellIndex);
+    ui->statusbar->addWidget(m_labCellType);
+    ui->statusbar->addWidget(m_labStudID);
+    ui->chkBoxHeadEdit->setChecked(Qt::Unchecked);
 }
 
 ExQTableWidget::~ExQTableWidget()
--- src/Runtime/Series/k_ExQTableWidget/ExQTableWidget.h
+++ src/Runtime/Series/k_ExQTableWidget/ExQTableWidget.h
@@ -2,10 +2,12 @@
 #define EXQTABLEWIDGET_H
 
 #include <QMainWindow>
-
+#include <qlabel.h>
 namespace Ui {
 class ExQTableWidget;
 }
+
+class QLabel;
 
 class ExQTableWidget : public QMainWindow
 {
@@ -17,6 +19,9 @@ public:
 
 private:
     Ui::ExQTableWidget *ui;
+    QLabel *m_labCellIndex;                              //状态栏上用来显示单元格的行号、列号
+    QLabel *m_labCellType;                               //状态栏上用来显示单元格的type
+    QLabel *m_labStudID;                                 //状态栏上用来显示单元格的学号
 };
 
 #endif // EXQTABLEWIDGET_H
