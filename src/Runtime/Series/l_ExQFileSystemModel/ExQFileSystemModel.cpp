1f3fe5eb8c3c54a74e5f48b8a0191a2790f944d6
StoneHappy <18017158729@163.com>
2022-11-10 22:22:52 +0800

    ✨ feat: Qfile system

--- src/Runtime/Series/l_ExQFileSystemModel/ExQFileSystemModel.cpp
+++ src/Runtime/Series/l_ExQFileSystemModel/ExQFileSystemModel.cpp
@@ -1,13 +1,65 @@
-#include "ExQFileSystemModel.h"
+﻿#include "ExQFileSystemModel.h"
 #include "ui_ExQFileSystemModel.h"
+
+#include <QFileSystemModel>
+#include <QLabel>
+#include <QCheckBox>
 
 ExQFileSystemModel::ExQFileSystemModel(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::ExQFileSystemModel)
 {
     ui->setupUi(this);
+    init();
+    connect(ui->treeView, &QTreeView::clicked, ui->listView, &QListView::setRootIndex);
+    connect(ui->treeView, &QTreeView::clicked, ui->tableView, &QTableView::setRootIndex);
 }
 
+void ExQFileSystemModel::init()
+{
+    //设置数据模型，且加载到各个视图上面
+    m_model = new QFileSystemModel(this);
+    QString currPath = QDir::currentPath();  //获取当前路径
+    m_model->setRootPath(currPath);          //设置根目录
+    ui->treeView->setModel(m_model);         //设置数据模型
+    ui->listView->setModel(m_model);
+    ui->tableView->setModel(m_model);
+    ui->columnView->setModel(m_model);
+
+    //初始化状态栏
+    m_labFileName = new QLabel(QString::fromLocal8Bit("名称："), ui->statusbar);
+    m_labFileName->setMinimumWidth(180);
+    m_labFileSize = new QLabel(QString::fromLocal8Bit("大小："), ui->statusbar);
+    m_labFileSize->setFixedWidth(130);
+    m_labFileType = new QLabel(QString::fromLocal8Bit("类型："), ui->statusbar);
+    m_labFileType->setFixedWidth(130);
+    m_labPath = new QLabel(QString::fromLocal8Bit("路径：") + currPath, ui->statusbar);
+    m_chkBoxIsFile = new QCheckBox(QString::fromLocal8Bit("当前为文件夹"), ui->statusbar);
+    m_chkBoxIsFile->setFixedWidth(130);
+
+    //各种QLable添加到状态栏
+    ui->statusbar->addWidget(m_labFileName);
+    ui->statusbar->addWidget(m_labFileSize);
+    ui->statusbar->addWidget(m_labFileType);
+    ui->statusbar->addWidget(m_chkBoxIsFile);
+    ui->statusbar->addWidget(m_labPath);
+}
+void ExQFileSystemModel::on_treeView_clicked(const QModelIndex& index)
+{
+    m_chkBoxIsFile->setChecked(m_model->isDir(index));                //是否是目录
+    m_labFileName->setText(QString::fromLocal8Bit("名称：") + m_model->fileName(index));      //文件名称
+    double size = m_model->size(index) / 1024.0;
+
+    if (size < 1024)
+        m_labFileSize->setText(QString::fromLocal8Bit("类型：") + QString::number(size, 'f', 2) + "KB");
+    else if (1024 <= size && size < 1024 * 1024)
+        m_labFileSize->setText(QString::fromLocal8Bit("类型：") + QString::number(size / 1024, 'f', 2) + "MB");
+    else
+        m_labFileSize->setText(QString::fromLocal8Bit("类型：") + QString::number(size / (1024 * 1024), 'f', 2) + "GB");
+
+    m_labFileType->setText(m_model->type(index));
+    m_labPath->setText(m_model->filePath(index));
+}
 ExQFileSystemModel::~ExQFileSystemModel()
 {
     delete ui;
--- src/Runtime/Series/l_ExQFileSystemModel/ExQFileSystemModel.h
+++ src/Runtime/Series/l_ExQFileSystemModel/ExQFileSystemModel.h
@@ -1,4 +1,4 @@
-#ifndef EXQFILESYSTEMMODEL_H
+﻿#ifndef EXQFILESYSTEMMODEL_H
 #define EXQFILESYSTEMMODEL_H
 
 #include <QMainWindow>
@@ -6,6 +6,10 @@
 namespace Ui {
 class ExQFileSystemModel;
 }
+
+class QLabel;
+class QCheckBox;
+class QFileSystemModel;
 
 class ExQFileSystemModel : public QMainWindow
 {
@@ -15,8 +19,21 @@ public:
     explicit ExQFileSystemModel(QWidget *parent = nullptr);
     ~ExQFileSystemModel();
 
+private slots:
+    void on_treeView_clicked(const QModelIndex& index);
+
+private:
+    void init();
+
 private:
     Ui::ExQFileSystemModel *ui;
+
+    QLabel* m_labFileName;       //文件名
+    QLabel* m_labFileSize;       //文件大小
+    QLabel* m_labFileType;       //文件类型
+    QLabel* m_labPath;           //路径
+    QCheckBox* m_chkBoxIsFile;   //当前是否为文件或文件夹
+    QFileSystemModel* m_model;   //设置文件系统的模型
 };
 
 #endif // EXQFILESYSTEMMODEL_H
