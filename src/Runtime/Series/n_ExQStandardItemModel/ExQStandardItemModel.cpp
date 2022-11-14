0c49939fe6f74975907c308ea6ded90f84ce2193
StoneHappy <18017158729@163.com>
2022-11-14 20:33:25 +0800

    ✨ feat: status bar

--- src/Runtime/Series/n_ExQStandardItemModel/ExQStandardItemModel.cpp
+++ src/Runtime/Series/n_ExQStandardItemModel/ExQStandardItemModel.cpp
@@ -1,11 +1,24 @@
 #include "ExQStandardItemModel.h"
 #include "ui_ExQStandardItemModel.h"
-
+#include <QToolButton>
 ExQStandardItemModel::ExQStandardItemModel(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::ExQStandardItemModel)
 {
     ui->setupUi(this);
+    setWindowTitle(QString::fromLocal8Bit("QTableView和QStandardItemModel的用法"));
+
+    ui->toolBar->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);     //设置主工具栏的图标样式风格
+
+    m_labCurrFile = new QLabel(QString::fromLocal8Bit("当前文件："), this);                       //设置状态栏
+    m_labCellPos = new QLabel(QString::fromLocal8Bit("当前单元格："), this);
+    m_labCellText = new QLabel(QString::fromLocal8Bit("单元格内容："), this);
+    m_labCurrFile->setMinimumWidth(200);
+    m_labCellPos->setMinimumWidth(200);
+    m_labCellText->setMinimumWidth(200);
+    ui->statusbar->addWidget(m_labCurrFile);
+    ui->statusbar->addWidget(m_labCellPos);
+    ui->statusbar->addWidget(m_labCellText);
 }
 
 ExQStandardItemModel::~ExQStandardItemModel()
--- src/Runtime/Series/n_ExQStandardItemModel/ExQStandardItemModel.h
+++ src/Runtime/Series/n_ExQStandardItemModel/ExQStandardItemModel.h
@@ -7,6 +7,10 @@ namespace Ui {
 class ExQStandardItemModel;
 }
 
+class QStandardItemModel;
+class QLabel;
+class QItemSelectionModel;
+
 class ExQStandardItemModel : public QMainWindow
 {
     Q_OBJECT
@@ -17,6 +21,12 @@ public:
 
 private:
     Ui::ExQStandardItemModel *ui;
+
+    QLabel* m_labCurrFile;                 //当前文件
+    QLabel* m_labCellPos;                   //当前单元格行列号
+    QLabel* m_labCellText;                  //当前单元格数据内容
+    QStandardItemModel* m_model;            //数据模型
+    QItemSelectionModel* m_selectModet;     //选择模型
 };
 
 #endif // EXQSTANDARDITEMMODEL_H
